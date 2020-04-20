2b820f8c0aa15ca0eb34ec4e12e092407980863b
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-19 21:20:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  let matchingDrivers = [];
+  for(const person of drivers) {
+    if(person.toLowerCase() === string.toLowerCase()) {
+      matchingDrivers.push(person);
+    }
+  }
+  return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+  let fuzzyMatchingDrivers = [];
+ 
+  for(const person of drivers) {
+    for(let i = 0; i < string.length; i++){
+      if (person[i] !== string[i]) {
+        i = string.length;
+      }
+      if (i === string.length - 1) {
+        fuzzyMatchingDrivers.push(person);
+      }
+    }
+  }
+  return fuzzyMatchingDrivers;
+}
+
+function matchName(drivers, string) {
+  let nameMatch = [];
+
+  for(const person of drivers) {
+    if (person['name'] === string) {
+      nameMatch.push(person);
+    }
+  }
+  return nameMatch;
+}
